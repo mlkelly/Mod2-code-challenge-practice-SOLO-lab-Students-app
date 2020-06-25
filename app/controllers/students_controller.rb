cefb2065760a91aa05f630cfb0b68e136f11e9b8
mlkelly <maurak87@gmail.com>
2020-06-25 11:36:16 -0400

    finished solo-lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,53 @@
 class StudentsController < ApplicationController
+  before_action :current_student, only: [:show, :update, :edit]
 
   def index
     @students = Student.all
   end
+
+  def show
+  end
+
+  def new
+    @instructors = Instructor.all
+    @student = Student.new
+  end
+
+  def create
+    student = Student.new(student_params)
+    # redirect_to "/instructors/#{student_params[:instructor_id]}"
+    if student.valid?
+      student.save
+      redirect_to "/instructors/#{student_params[:instructor_id]}"
+    else
+      flash[:errors] = student.errors.full_messages
+      redirect_to new_student_path
+    end
+  end
+
+  def edit
+    @instructors = Instructor.all
+  end
+
+  def update 
+    @student.update(student_params)
+    redirect_to student_path
+    # redirect_to edit_student_path
+    # if @student.valid?
+    #   @student.save
+    #   redirect_to @student
+    # else
+    #   # flash[:errors] = @student.errors.full_messages
+    #   redirect_to edit_student_path
+    # end
+  end
   
+  private
+  def current_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :major, :age, :instructor_id)
+  end
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,15 @@
 class Instructor < ApplicationRecord
     has_many :students
+
+    def avg_student_age
+        if students.count != 0
+            students.sum {|s| s.age} / students.count
+        else
+            0
+        end
+    end
+
+    def name_alphabetical_sort
+        students.sort_by {|s| s.name}
+    end
 end
